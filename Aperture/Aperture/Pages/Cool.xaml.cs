9f33b37eca4915dfa8bb1792dd80fecf6132585b
Marius <marius.muntean@live.com>
2019-07-28 20:10:55 +0200

    Enhancing the sample

--- Aperture/Aperture/Pages/Cool.xaml.cs
+++ Aperture/Aperture/Pages/Cool.xaml.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Aperture.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+    [DesignTimeVisible(true)]
     public partial class Cool : ContentPage
     {
         public Cool()
--- Aperture/Aperture/Pages/Simple.xaml.cs
+++ Aperture/Aperture/Pages/Simple.xaml.cs
@@ -1,9 +1,11 @@
-﻿using Xamarin.Forms;
+﻿using System.ComponentModel;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Aperture.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+    [DesignTimeVisible(true)]
     public partial class Simple : ContentPage
     {
         public Simple()
